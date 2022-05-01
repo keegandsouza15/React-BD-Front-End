20c883480d89476460d531d6c8819f36cea22fcd
kkeegandsouza15 <keegan0415dsouza@gmail.com>
2022-04-30 19:26:35 -0500

    Added score table functionality

--- src/components/HighScoresTable.js
+++ src/components/HighScoresTable.js
@@ -1,58 +1,57 @@
-const HighScoreTable = () => {
+import React from "react"
+
+function ScoreList(props){
+    const scores = props.scores;
+    console.log(scores)
+    const listItems = scores.map((item) => 
+        <tr key={item[0]}>
+            <td>{item[0]}</td>
+            <td>{item[1]}</td>
+        </tr>
+    )
 
     return (
-        <div className='w3-container w3-display-left' style={{width:"50%", height:"100%"}}>
-            <div className='w3-display-container w3-amber w3-display-right w3-margin' style={{width:"400px"}}>
-                <table className="w3-table">
-                    <tr>
-                        <th>Username</th>
-                        <th>Score</th>
-                    </tr>
-                    <tr>
-                        <td>placeholder</td>
-                        <td>0</td>
-                    </tr>
-                    <tr>
-                        <td>placeholder</td>
-                        <td>0</td>
-                    </tr>
-                    <tr>
-                        <td>placeholder</td>
-                        <td>0</td>
-                    </tr>
-                    <tr>
-                        <td>placeholder</td>
-                        <td>0</td>
-                    </tr>
-                    <tr>
-                        <td>placeholder</td>
-                        <td>0</td>
-                    </tr>
-                    <tr>
-                        <td>placeholder</td>
-                        <td>0</td>
-                    </tr>
-                    <tr>
-                        <td>placeholder</td>
-                        <td>0</td>
-                    </tr>
-                    <tr>
-                        <td>placeholder</td>
-                        <td>0</td>
-                    </tr>
-                    <tr>
-                        <td>placeholder</td>
-                        <td>0</td>
-                    </tr>
-                    <tr>
-                        <td>placeholder</td>
-                        <td>0</td>
-                    </tr>
-                </table>
-            </div>
-           
-        </div>        
+        <tbody>{listItems}</tbody>
     )
 }
+
+class HighScoreTable extends React.Component {
+    constructor(props) {
+        super(props)
+        this.api = "http://localhost:5000/HighScores/GetTopTen"
+        this.state = {scores: [['dummy', 0], ['test', 1], ['bob', 2]]}
+    }
+
+    getScores = async () => {
+        const response = await fetch(this.api)
+        const data = await response.json();
+
+        this.setState({scores: data})
+    }
+
+    render() {
+        return (
+            <div className='w3-container w3-display-left' style={{width:"50%", height:"100%"}}>
+            <div className='w3-display-container w3-amber w3-display-right w3-margin' style={{width:"400px"}}>
+                
+                <table className="w3-table">
+                    <tbody>
+                        <tr>
+                            <th>Username</th>
+                            <th>Score</th>
+                        </tr>
+                        
+                    </tbody>
+                    <ScoreList scores={this.state.scores}/>
+                    
+                </table>
+            </div>
+            <button onClick={this.getScores}>Get Scores</button>
+            </div>        
+        )
+    }
+
+}
+
 
 export default HighScoreTable
